eb259471829e499d46f568518a2b70596b1dd581
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-01-29 22:23:49 +0100

    finalize version with byte array  - working solution

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,13 +7,21 @@ class Solution {
 
         if (grid.length == 0) return 0;
 
+        byte[][] gridAsByte = new byte[grid.length][grid[0].length];
+
         int islandsCount = 0;
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
+                gridAsByte[i][j] = (grid[i][j] == '1') ? (byte)1 : (byte)0;
+            }
+        }
+
+        for (int i = 0; i < gridAsByte.length; i++) {
+            for (int j = 0; j < gridAsByte[i].length; j++) {
+                if (gridAsByte[i][j] == (byte)1) {
                     islandsCount++;
-                    markIslandPieces(i, j, grid);
+                    markIslandPieces(i, j, gridAsByte);
                 }
             }
         }
@@ -21,25 +29,25 @@ class Solution {
         return islandsCount;
     }
 
-    private void markIslandPieces(int i, int j, char[][] grid) {
-        if (grid[i][j] == '2' || grid[i][j] == '0') return;
+    private void markIslandPieces(int i, int j, byte[][] gridAsByte) {
+        if (gridAsByte[i][j] == (byte)2 || gridAsByte[i][j] == (byte)0) return;
 
-        grid[i][j] = '2';
+        gridAsByte[i][j] = (byte)2;
 
         if (i - 1 >= 0) {
-            markIslandPieces(i - 1, j, grid);
+            markIslandPieces(i - 1, j, gridAsByte);
         }
 
-        if (j + 1 < grid[i].length) {
-            markIslandPieces(i , j + 1, grid);
+        if (j + 1 < gridAsByte[i].length) {
+            markIslandPieces(i , j + 1, gridAsByte);
         }
 
-        if (i + 1 < grid.length) {
-            markIslandPieces(i + 1, j, grid);
+        if (i + 1 < gridAsByte.length) {
+            markIslandPieces(i + 1, j, gridAsByte);
         }
 
         if (j - 1 >= 0) {
-            markIslandPieces(i, j - 1, grid);
+            markIslandPieces(i, j - 1, gridAsByte);
         }
     }
 }
